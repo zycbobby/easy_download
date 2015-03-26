0e83ce0abb09d163b689e5c9171ae7e9166107dd
zycbobby <zuoyuchu@gmail.com>
2015-03-26 14:00:27 +0800

    I think the data structure is OK now

--- server/api/thing/thing.model.js
+++ server/api/thing/thing.model.js
@@ -1,32 +1,72 @@
 'use strict';
 
 var mongoose = require('mongoose'),
-    Schema = mongoose.Schema,
-    validate = require('mongoose-validator');
+  Schema = mongoose.Schema,
+  validate = require('mongoose-validator');
 
 var ThingSchema = new Schema({
   name: String,
   info: String,
-  url : {
-    type :String,
-    validate : validate({
+  url: {
+    type: String,
+    validate: validate({
       validator: 'isURL'
     })
   },
-  thumbsUp: {
-    type : Number,
-    'default' : 0
+  ownerInfo : {
+    userId: {
+      required : true,
+      type : mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    }
   },
-  thumbsDown: {
-    type : Number,
-    'default' : 0
+  otherInfo : {
+    thumbs : {
+      up: {
+        type: Number,
+        'default': 0
+      },
+      down: {
+        type: Number,
+        'default': 0
+      }
+    },
+    comments: [{
+      userId: mongoose.Schema.Types.ObjectId,
+      comment: {
+        text : String,
+        createdAt : Date,
+        valid : {
+          type : Boolean,
+          'default' : true
+        }
+      }
+    }],
+    tags : [
+      {
+        tagId : mongoose.Schema.Types.ObjectId
+      }
+    ]
   },
-  comments : [{
-    userId : mongoose.Schema.Types.ObjectId,
-    comment : String
-  }],
-  active: Boolean
+  active: {
+    type: Boolean,
+    'default': true
+  },
+  verified: {
+    type: Boolean,
+    'default': false
+  },
+  createdAt : Date,
+  updatedAt : Date
+});
 
+ThingSchema.pre('save', function(next){
+  this.updatedAt = Date.now();
+  if (this.isNew) {
+    this.createdAt = Date.now();
+  }
+
+  next();
 });
 
 module.exports = mongoose.model('Thing', ThingSchema);
--- server/config/seed.js
+++ server/config/seed.js
@@ -4,46 +4,40 @@
  */
 
 'use strict';
-
+var mongoose = require('mongoose-q')(require('mongoose'));
 var Thing = require('../api/thing/thing.model');
 var User = require('../api/user/user.model');
 
-Thing.find({}).remove(function() {
-  Thing.create({
-    name : 'Development Tools',
-    info : 'Integration with popular tools such as Bower, Grunt, Karma, Mocha, JSHint, Node Inspector, Livereload, Protractor, Jade, Stylus, Sass, CoffeeScript, and Less.'
-  }, {
-    name : 'Server and Client integration',
-    info : 'Built with a powerful and fun stack: MongoDB, Express, AngularJS, and Node.'
-  }, {
-    name : 'Smart Build System',
-    info : 'Build system ignores `spec` files, allowing you to keep tests alongside code. Automatic injection of scripts and styles into your index.html'
-  },  {
-    name : 'Modular Structure',
-    info : 'Best practice client and server structures allow for more code reusability and maximum scalability'
-  },  {
-    name : 'Optimized Build',
-    info : 'Build process packs up your templates as a single JavaScript payload, minifies your scripts/css/images, and rewrites asset names for caching.'
-  },{
-    name : 'Deployment Ready',
-    info : 'Easily deploy your app to Heroku or Openshift with the heroku and openshift subgenerators'
-  });
-});
-
-User.find({}).remove(function() {
-  User.create({
-    provider: 'local',
-    name: 'Test User',
-    email: 'test@test.com',
-    password: 'test'
-  }, {
-    provider: 'local',
-    role: 'admin',
-    name: 'Admin',
-    email: 'admin@admin.com',
-    password: 'admin'
-  }, function() {
-      console.log('finished populating users');
+User.findQ().then(function(data) {
+  return User.removeQ();
+}).then(function(){
+  return User.createQ(
+    {
+      provider: 'local',
+      name: 'Test User',
+      email: 'test@test.com',
+      password: 'test'
+    }, {
+      provider: 'local',
+      role: 'admin',
+      name: 'Admin',
+      email: 'admin@admin.com',
+      password: 'admin'
     }
-  );
-});
+  )
+}).then(function(){
+  return Thing.removeQ();
+}).then(function(){
+  return User.findOneQ({
+    name : 'Admin'
+  });
+}).then(function(admin) {
+  return Thing.createQ({
+    name : 'Development Tools',
+    info : 'Integration with popular tools such as Bower, Grunt, Karma, Mocha, JSHint, Node Inspector, Livereload, Protractor, Jade, Stylus, Sass, CoffeeScript, and Less.',
+    url : 'http://www.baidu.com',
+    ownerInfo : {
+      userId : admin._id
+    }
+  });
+}).done();
