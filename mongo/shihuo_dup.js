9a21ceec83635a4bb5938af63f6cd78471eca8cc
zycbobby <zuoyuchu@gmail.com>
2015-04-01 15:03:23 +0800

    #5 es docker file, this docker file still have problem

--- mongo/shihuo_dup.js
+++ mongo/shihuo_dup.js
@@ -1,19 +1,16 @@
 db.oldItems.mapReduce(
     function(){ 
+        this.crawled = false;
         if ( this.url.indexOf('shihuo') > -1) {
-            emit(this.url.substring(0, this.url.indexOf('#')), this);    
+            emit(this.url.substring(0, this.url.indexOf('#')), this);
         } else {
             emit(this.url, this);    
+            
         }
         
     }, 
     function(key, values){ 
-        if ( key.indexOf('shihuo') > -1) {
-            return values[0];
-        } else {
-            console.log('it should never reach');
-            return values[0];
-        }
+        return values[0];
     }, 
     { 
         query : {}, 
