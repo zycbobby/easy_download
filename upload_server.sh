aeb43625bb0b4d9a9f18ce3b1c7f5f354639b8b7
zycbobby <zuoyuchu@gmail.com>
2015-04-07 16:56:16 +0800

    Fix upload script, remove the folder first

--- upload_server.sh
+++ upload_server.sh
@@ -1,3 +1,6 @@
+# Noted that you have remove the remo
+ssh root@misscatandzuozuo.info rm -rf /mnt/ext/easy_download/
+ssh root@misscatandzuozuo.info mkdir /mnt/ext/easy_download/
 scp -r ./dist/ root@misscatandzuozuo.info:/mnt/ext/easy_download/dist
 scp ./Dockerfile root@misscatandzuozuo.info:/mnt/ext/easy_download/Dockerfile
 scp ./docker-compose.yml root@misscatandzuozuo.info:/mnt/ext/easy_download/docker-compose.yml
