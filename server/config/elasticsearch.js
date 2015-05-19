38e85d72c4e59b273c4cc1175279c60233653029
zycbobby <zuoyuchu@gmail.com>
2015-05-19 13:12:14 +0800

    Add process.env.NODE_ENV

--- server/config/elasticsearch.js
+++ server/config/elasticsearch.js
@@ -1,5 +1,7 @@
 'use strict';
 
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
 var Thing = require('../api/thing/thing.model');
 var es = require('elasticsearch');
 var esConfig = require('./environment').elasticSearch;
