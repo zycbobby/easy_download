ddd8a5bd2e2079b54e6935af460a91ae1708d697
zycbobby <zuoyuchu@gmail.com>
2015-05-20 18:07:33 +0800

    Fix upload server script

--- upload_server.sh
+++ upload_server.sh
@@ -5,7 +5,7 @@ ssh root@misscatandzuozuo.info mkdir /mnt/ext/easy_download/elasticsearch
 scp -r ./dist/ root@misscatandzuozuo.info:/mnt/ext/easy_download/dist
 scp ./Dockerfile root@misscatandzuozuo.info:/mnt/ext/easy_download/Dockerfile
 scp ./docker-compose.yml root@misscatandzuozuo.info:/mnt/ext/easy_download/docker-compose.yml
-scp ./elasticsearch/Dockerfile root@misscatandzuozuo.info:/mnt/ext/easy_download/elasticsearch/Dockerfile
+scp -r ./elasticsearch/ root@misscatandzuozuo.info:/mnt/ext/easy_download/elasticsearch/
 
 
 # What you should do after uploading
