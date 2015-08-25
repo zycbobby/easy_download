b2750a0aeda0ecc46a4e0ba22994bffe50e26e89
zycbobby <zuoyuchu@gmail.com>
2015-08-25 18:26:47 +0800

    Add travis mongodb url

--- server/config/environment/test.js
+++ server/config/environment/test.js
@@ -5,7 +5,7 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost:27017/easydownload-test'
+    uri: process.env.MONGOLAB_URI || 'mongodb://localhost:27017/easydownload-test'
   },
 
   itemCron: '*/5 * * * * *',
